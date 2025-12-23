45ae1e7daed4d6a38dc252c2b7cfcb314c958c15
Brian Richwine <brichwin@iu.edu>
2025-12-23 14:51:24 -0500

    Changes to update to MathCAT Version: 0.7.6-beta.1

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    // Check if the target family is "wasm"
-    if std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap_or_default() == "wasm" {
-        // Set the target family to "wasm"
-        println!("cargo:rustc-cfg=target_family=\"wasm\"");
-    }
-}
