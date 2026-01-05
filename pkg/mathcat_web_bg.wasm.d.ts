913d24936a0dbc1ade0111fc6e760e966e0b9ac9
Brian Richwine <brichwin@iu.edu>
2026-01-05 14:47:27 -0500

    add pkg folder

---
+++ pkg/mathcat_web_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_mathcat_free: (a: number, b: number) => void;
+export const mathcat_getBraille: (a: number, b: number) => [number, number, number, number];
+export const mathcat_getPreference: (a: number, b: number) => [number, number, number, number];
+export const mathcat_getSpokenText: () => [number, number, number, number];
+export const mathcat_getVersion: () => [number, number];
+export const mathcat_setMathML: (a: number, b: number) => [number, number, number, number];
+export const mathcat_setPreference: (a: number, b: number, c: number, d: number) => [number, number];
+export const mathcat_setRulesDir: (a: number, b: number) => [number, number];
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
